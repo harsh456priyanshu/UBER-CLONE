38762bb7b9a1948ff75a7d652a072675db3285f0
harsh456priyanshu <harshpriyanshu456@gmail.com>
2025-06-07 16:09:32 +0530

    Ride , Map and oth. work done

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,8 @@ const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
 const morgan = require('morgan');
-
+const mapsRoutes =require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');         
 
 connectToDb();
 
@@ -31,5 +32,7 @@ app.get( '/', (req, res) => {
 
 app.use('/users' , userRoutes);
 app.use('/captains' , captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides' , rideRoutes)
 
 module.exports = app;
---
+++ Backend/routes/ride.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+ const { body } = require('express-validator');
+ const rideController  = require('../controllers/ride.controller');
+ const authMiddleware = require('../middlewares/auth.middleware');
+
+router.post('/create',
+    authMiddleware.authUser,
+    body('pickup').isString().isLength({ min: 3}).withMessage('Invalid Pickup address'),
+    body('destination').isString().isLength({ min: 3}).withMessage('Invalid destinaion address'),
+    body('vehicleType').isString().isIn(['auto' , 'car' , 'moto' ]).withMessage('Invalid vehicle Type'),
+    rideController.createRide
+)
+
+
+
+module.exports = router;
