5b5052ec50845913cd90bfde63ee0241dac08d99
harsh456priyanshu <harshpriyanshu456@gmail.com>
2025-06-15 18:07:39 +0530

    Socket connection establish and other

--- frontend/src/components/VehiclePanel.jsx
+++ frontend/src/components/VehiclePanel.jsx
@@ -10,6 +10,7 @@ const VehiclePanel = (props) => {
 
           <div onClick={() => {
             props.setConfirmRidePanel(true)
+            props.selectVehicle('car')
           }} className='flex border-2 active:border-black mb-2 rounded-xl p-3 w-full item-center justify-between'>
             
             <img className='h-12' src='https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg.webp' alt='' />
@@ -18,11 +19,12 @@ const VehiclePanel = (props) => {
               <h5 className='font-medium text-sm' >2 mins away</h5>
               <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
             </div>
-            <h2 className='text-lg font-semibold'>₹193.20</h2>
+             <h2 className='text-lg font-semibold'>₹{props.fare.car}</h2>
           </div>
 
           <div onClick={() => {
             props.setConfirmRidePanel(true)
+             props.selectVehicle('moto')
           }} className='flex border-2 active:border-black mb-2 rounded-xl p-3 w-full item-center justify-between'>
             
             <img className='h-12' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png' alt='' />
@@ -31,11 +33,12 @@ const VehiclePanel = (props) => {
               <h5 className='font-medium text-sm' >3 mins away</h5>
               <p className='font-normal text-xs text-gray-600'>Affordable, Motorcycle rides</p>
             </div>
-            <h2 className='text-lg font-semibold'>₹65.10</h2>
+            <h2 className='text-lg font-semibold'>₹{props.fare.moto}</h2>
           </div>
 
           <div onClick={() => {
             props.setConfirmRidePanel(true)
+             props.selectVehicle('auto')
           }} className='flex border-2 active:border-black mb-2 rounded-xl p-3 w-full item-center justify-between'>
             
             <img className='h-12' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png' alt='' />
@@ -44,7 +47,7 @@ const VehiclePanel = (props) => {
               <h5 className='font-medium text-sm' >2 mins away</h5>
               <p className='font-normal text-xs text-gray-600'>Affordable, Auto rides</p>
             </div>
-            <h2 className='text-lg font-semibold'>₹118.80</h2>
+            <h2 className='text-lg font-semibold'>₹{props.fare.auto}</h2>
           </div>
 
     </div>
